4d2b2af74b06c02eb6ea198423e34749aee4be19
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-10-19 16:33:32 +0800

    update

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -42,7 +42,7 @@
                         <a class="mobile-menu waves-effect waves-light" id="mobile-collapse" href="#!">
                             <i class="feather icon-toggle-right"></i>
                         </a>
-                        <a href="index.html">
+                        <a href="<?= admin_url() ?>">
                             <img class="img-fluid" src="../files/assets/images/logo.png" alt="Theme-Logo" />
                         </a>
                         <a class="mobile-options waves-effect waves-light">
@@ -84,7 +84,7 @@
                                         </li>
                                         <li>
                                             <div class="media">
-                                                <img class="img-radius" src="../files/assets/images/avatar-4.jpg" alt="Generic placeholder image">
+                                                <img class="img-radius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRoWrihcOLGik2xRG96dL0F4HjA71lPywi2w&usqp=CAU" alt="Generic placeholder image">
                                                 <div class="media-body">
                                                     <h5 class="notification-user">John Doe</h5>
                                                     <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
@@ -104,7 +104,7 @@
                                         </li>
                                         <li>
                                             <div class="media">
-                                                <img class="img-radius" src="../files/assets/images/avatar-4.jpg" alt="Generic placeholder image">
+                                                <img class="img-radius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRoWrihcOLGik2xRG96dL0F4HjA71lPywi2w&usqp=CAU" alt="Generic placeholder image">
                                                 <div class="media-body">
                                                     <h5 class="notification-user">Sara Soudein</h5>
                                                     <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
@@ -115,18 +115,10 @@
                                     </ul>
                                 </div>
                             </li>
-                            <li class="header-notification">
-                                <div class="dropdown-primary dropdown">
-                                    <div class="displayChatbox dropdown-toggle" data-toggle="dropdown">
-                                        <i class="feather icon-message-square"></i>
-                                        <span class="badge bg-c-green">3</span>
-                                    </div>
-                                </div>
-                            </li>
                             <li class="user-profile header-notification">
                                 <div class="dropdown-primary dropdown">
                                     <div class="dropdown-toggle" data-toggle="dropdown">
-                                        <img src="../files/assets/images/avatar-4.jpg" class="img-radius" alt="User-Profile-Image">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRoWrihcOLGik2xRG96dL0F4HjA71lPywi2w&usqp=CAU" class="img-radius" alt="User-Profile-Image">
                                         <span>John Doe</span>
                                         <i class="feather icon-chevron-down"></i>
                                     </div>
@@ -152,7 +144,7 @@
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="auth-normal-sign-in.html">
+                                            <a href="<?= logout_url() ?>">
                                                 <i class="feather icon-log-out"></i> Logout
                                             </a>
                                         </li>
@@ -164,136 +156,6 @@
                 </div>
             </nav>
 
-            <div id="sidebar" class="users p-chat-user showChat">
-                <div class="had-container">
-                    <div class="p-fixed users-main">
-                        <div class="user-box">
-                            <div class="chat-search-box">
-                                <a class="back_friendlist">
-                                    <i class="feather icon-x"></i>
-                                </a>
-                                <div class="right-icon-control">
-                                    <form class="form-material">
-                                        <div class="form-group form-primary">
-                                            <input type="text" name="footer-email" class="form-control" id="search-friends" required="">
-                                            <span class="form-bar"></span>
-                                            <label class="float-label">
-                                                <i class="feather icon-search m-r-10"></i>Search Friend
-                                            </label>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="main-friend-list">
-                                <div class="media userlist-box waves-effect waves-light" data-id="1" data-status="online" data-username="Josephin Doe">
-                                    <a class="media-left" href="#!">
-                                        <img class="media-object img-radius img-radius" src="../files/assets/images/avatar-3.jpg" alt="Generic placeholder image ">
-                                        <div class="live-status bg-success"></div>
-                                    </a>
-                                    <div class="media-body">
-                                        <div class="chat-header">Josephin Doe</div>
-                                    </div>
-                                </div>
-                                <div class="media userlist-box waves-effect waves-light" data-id="2" data-status="online" data-username="Lary Doe">
-                                    <a class="media-left" href="#!">
-                                        <img class="media-object img-radius" src="../files/assets/images/avatar-2.jpg" alt="Generic placeholder image">
-                                        <div class="live-status bg-success"></div>
-                                    </a>
-                                    <div class="media-body">
-                                        <div class="f-13 chat-header">Lary Doe</div>
-                                    </div>
-                                </div>
-                                <div class="media userlist-box waves-effect waves-light" data-id="3" data-status="online" data-username="Alice">
-                                    <a class="media-left" href="#!">
-                                        <img class="media-object img-radius" src="../files/assets/images/avatar-4.jpg" alt="Generic placeholder image">
-                                        <div class="live-status bg-success"></div>
-                                    </a>
-                                    <div class="media-body">
-                                        <div class="f-13 chat-header">Alice</div>
-                                    </div>
-                                </div>
-                                <div class="media userlist-box waves-effect waves-light" data-id="4" data-status="offline" data-username="Alia">
-                                    <a class="media-left" href="#!">
-                                        <img class="media-object img-radius" src="../files/assets/images/avatar-3.jpg" alt="Generic placeholder image">
-                                        <div class="live-status bg-default"></div>
-                                    </a>
-                                    <div class="media-body">
-                                        <div class="f-13 chat-header">Alia<small class="d-block text-muted">10 min ago</small></div>
-                                    </div>
-                                </div>
-                                <div class="media userlist-box waves-effect waves-light" data-id="5" data-status="offline" data-username="Suzen">
-                                    <a class="media-left" href="#!">
-                                        <img class="media-object img-radius" src="../files/assets/images/avatar-2.jpg" alt="Generic placeholder image">
-                                        <div class="live-status bg-default"></div>
-                                    </a>
-                                    <div class="media-body">
-                                        <div class="f-13 chat-header">Suzen<small class="d-block text-muted">15 min ago</small></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="showChat_inner">
-                <div class="media chat-inner-header">
-                    <a class="back_chatBox">
-                        <i class="feather icon-x"></i> Josephin Doe
-                    </a>
-                </div>
-                <div class="main-friend-chat">
-                    <div class="media chat-messages">
-                        <a class="media-left photo-table" href="#!">
-                            <img class="media-object img-radius img-radius m-t-5" src="../files/assets/images/avatar-2.jpg" alt="Generic placeholder image">
-                        </a>
-                        <div class="media-body chat-menu-content">
-                            <div class="">
-                                <p class="chat-cont">I'm just looking around. Will you tell me something about yourself?</p>
-                            </div>
-                            <p class="chat-time">8:20 a.m.</p>
-                        </div>
-                    </div>
-                    <div class="media chat-messages">
-                        <div class="media-body chat-menu-reply">
-                            <div class="">
-                                <p class="chat-cont">Ohh! very nice</p>
-                            </div>
-                            <p class="chat-time">8:22 a.m.</p>
-                        </div>
-                    </div>
-                    <div class="media chat-messages">
-                        <a class="media-left photo-table" href="#!">
-                            <img class="media-object img-radius img-radius m-t-5" src="../files/assets/images/avatar-2.jpg" alt="Generic placeholder image">
-                        </a>
-                        <div class="media-body chat-menu-content">
-                            <div class="">
-                                <p class="chat-cont">can you come with me?</p>
-                            </div>
-                            <p class="chat-time">8:20 a.m.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-reply-box">
-                    <div class="right-icon-control">
-                        <form class="form-material">
-                            <div class="form-group form-primary">
-                                <input type="text" name="footer-email" class="form-control" required="">
-                                <span class="form-bar"></span>
-                                <label class="float-label">
-                                    <i class="feather icon-search m-r-10"></i>Share Your Thoughts
-                                </label>
-                            </div>
-                        </form>
-                        <div class="form-icon ">
-                            <button class="btn btn-success btn-icon  waves-effect waves-light">
-                                <i class="feather icon-message-circle"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
             <div class="pcoded-main-container">
                 <div class="pcoded-wrapper">
                     <!-- [ navigation menu ] start -->
@@ -301,7 +163,7 @@
                         <div class="pcoded-inner-navbar main-menu">
                             <div class="">
                                 <div class="main-menu-header">
-                                    <img class="img-menu-user img-radius" src="../files/assets/images/avatar-4.jpg" alt="User-Profile-Image">
+                                    <img class="img-menu-user img-radius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRoWrihcOLGik2xRG96dL0F4HjA71lPywi2w&usqp=CAU" alt="User-Profile-Image">
                                     <div class="user-details">
                                         <p id="more-details">John Doe<i class="feather icon-chevron-down m-l-10"></i></p>
                                     </div>
@@ -331,7 +193,7 @@
                                     </a>
                                     <ul class="pcoded-submenu">
                                         <li class="active">
-                                            <a href="index.html" class="waves-effect waves-dark">
+                                            <a href="<?= admin_url() ?>" class="waves-effect waves-dark">
                                                 <span class="pcoded-mtext">Default</span>
                                             </a>
                                         </li>
@@ -1234,7 +1096,7 @@
                                     </ul>
                                 </li>
                                 <li class="">
-                                    <a href="../files/extra-pages/landingpage/index.html" target="_blank" class="waves-effect waves-dark">
+                                    <a href="../files/extra-pages/landingpage/<?= admin_url() ?>" target="_blank" class="waves-effect waves-dark">
                                         <span class="pcoded-micon">
                                             <i class="feather icon-navigation-2"></i>
                                         </span>
