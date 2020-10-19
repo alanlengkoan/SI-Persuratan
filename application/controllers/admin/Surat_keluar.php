928cccacd69a618a6135808b4aab57ea7544a3ca
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-10-19 22:55:07 +0800

    tambah surat masuk dan keluar

---
+++ application/controllers/admin/Surat_keluar.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Surat_keluar extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data = [
+            'halaman'    => 'Surat Keluar',
+            'breadcrumb' => breadcrumb(admin_url()),
+            'content'    => 'admin/manajeman_user/view',
+            'css'        => '',
+            'js'         => ''
+        ];
+
+        $this->load->view('admin/base', $data);
+    }
+}
