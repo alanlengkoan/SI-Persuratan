d8327776028308daab2e931a531163f55d8edeeb
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-10-29 11:19:37 +0800

    cru master instansi

---
+++ application/controllers/admin/Instansi.php
@@ -0,0 +1,101 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Instansi extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // untuk load model
+        $this->load->model('crud');
+        $this->load->model('m_instansi');
+
+        // untuk cek session
+        checking_session($this->session->userdata());
+    }
+
+    public function index()
+    {
+        $data = [
+            'halaman'    => 'Instansi',
+            'breadcrumb' => breadcrumb(admin_url()),
+            'content'    => 'admin/instansi/view',
+            'data'       => $this->m_instansi->getAll(),
+            'css'        => 'admin/instansi/css/view',
+            'js'         => 'admin/instansi/js/view',
+        ];
+
+        $this->load->view('admin/base', $data);
+    }
+
+    // untuk ambil data by id
+    public function get()
+    {
+        $post   = $this->input->post(NULL, TRUE);
+        $result = $this->crud->gda('instansi', ['kd' => $post['id']]);
+
+        $data = [
+            'id_instansi' => $result['id_instansi'],
+            'kd'          => $result['kd'],
+            'nama'        => $result['nama'],
+            'keterangan'  => $result['keterangan'],
+        ];
+
+        // untuk reponse
+        $this->_response($data);
+    }
+
+    // untuk simpan data
+    public function add()
+    {
+        $post = $this->input->post(NULL, TRUE);
+
+        $data = [
+            'kd'         => $post['inpkode'],
+            'nama'       => $post['inpnama'],
+            'keterangan' => $post['inpketerangan'],
+            'ins'        => date('Y-m-d H:i'),
+            'ins_id'     => $this->session->userdata('id'),
+            'upd_id'     => $this->session->userdata('id'),
+        ];
+
+        $this->db->trans_start();
+        $this->crud->i('instansi', $data);
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === FALSE) {
+            $response = ['title' => 'Gagal!', 'text' => 'Gagal Simpan!', 'type' => 'error', 'button' => 'Ok!'];
+        } else {
+            $response = ['title' => 'Berhasil!', 'text' => 'Berhasil Simpan!', 'type' => 'success', 'button' => 'Ok!'];
+        }
+
+        // untuk reponse
+        $this->_response($response);
+    }
+
+    // untuk ubah data
+    public function upd()
+    {
+        $post = $this->input->post(NULL, TRUE);
+
+        $data = [
+            'nama'       => $post['inpnama'],
+            'keterangan' => $post['inpketerangan'],
+            'upd_id'     => $this->session->userdata('id'),
+        ];
+
+        $this->db->trans_start();
+        $this->crud->u('instansi', $data, ['kd' => $post['inpkode']]);
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === FALSE) {
+            $response = ['title' => 'Gagal!', 'text' => 'Gagal Simpan!', 'type' => 'error', 'button' => 'Ok!'];
+        } else {
+            $response = ['title' => 'Berhasil!', 'text' => 'Berhasil Simpan!', 'type' => 'success', 'button' => 'Ok!'];
+        }
+
+        // untuk reponse
+        $this->_response($response);
+    }
+}
