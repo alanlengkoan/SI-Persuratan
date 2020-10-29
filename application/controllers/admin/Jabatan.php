feddedc7ffb719fd8c344f443970a89b109e30db
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-10-29 11:30:12 +0800

    sru master jabatan

---
+++ application/controllers/admin/Jabatan.php
@@ -0,0 +1,101 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Jabatan extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // untuk load model
+        $this->load->model('crud');
+        $this->load->model('m_jabatan');
+
+        // untuk cek session
+        checking_session($this->session->userdata());
+    }
+
+    public function index()
+    {
+        $data = [
+            'halaman'    => 'Jabatan',
+            'breadcrumb' => breadcrumb(admin_url()),
+            'content'    => 'admin/jabatan/view',
+            'data'       => $this->m_jabatan->getAll(),
+            'css'        => 'admin/jabatan/css/view',
+            'js'         => 'admin/jabatan/js/view',
+        ];
+
+        $this->load->view('admin/base', $data);
+    }
+
+    // untuk ambil data by id
+    public function get()
+    {
+        $post   = $this->input->post(NULL, TRUE);
+        $result = $this->crud->gda('jabatan', ['kd' => $post['id']]);
+
+        $data = [
+            'id_jabatan' => $result['id_jabatan'],
+            'kd'          => $result['kd'],
+            'nama'        => $result['nama'],
+            'keterangan'  => $result['keterangan'],
+        ];
+
+        // untuk reponse
+        $this->_response($data);
+    }
+
+    // untuk simpan data
+    public function add()
+    {
+        $post = $this->input->post(NULL, TRUE);
+
+        $data = [
+            'kd'         => $post['inpkode'],
+            'nama'       => $post['inpnama'],
+            'keterangan' => $post['inpketerangan'],
+            'ins'        => date('Y-m-d H:i'),
+            'ins_id'     => $this->session->userdata('id'),
+            'upd_id'     => $this->session->userdata('id'),
+        ];
+
+        $this->db->trans_start();
+        $this->crud->i('jabatan', $data);
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === FALSE) {
+            $response = ['title' => 'Gagal!', 'text' => 'Gagal Simpan!', 'type' => 'error', 'button' => 'Ok!'];
+        } else {
+            $response = ['title' => 'Berhasil!', 'text' => 'Berhasil Simpan!', 'type' => 'success', 'button' => 'Ok!'];
+        }
+
+        // untuk reponse
+        $this->_response($response);
+    }
+
+    // untuk ubah data
+    public function upd()
+    {
+        $post = $this->input->post(NULL, TRUE);
+
+        $data = [
+            'nama'       => $post['inpnama'],
+            'keterangan' => $post['inpketerangan'],
+            'upd_id'     => $this->session->userdata('id'),
+        ];
+
+        $this->db->trans_start();
+        $this->crud->u('jabatan', $data, ['kd' => $post['inpkode']]);
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === FALSE) {
+            $response = ['title' => 'Gagal!', 'text' => 'Gagal Simpan!', 'type' => 'error', 'button' => 'Ok!'];
+        } else {
+            $response = ['title' => 'Berhasil!', 'text' => 'Berhasil Simpan!', 'type' => 'success', 'button' => 'Ok!'];
+        }
+
+        // untuk reponse
+        $this->_response($response);
+    }
+}
---
+++ application/views/admin/jabatan/js/view.php
@@ -0,0 +1,115 @@
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?= assets_url() ?>admin/assets/pages/data-table/js/jszip.min.js"></script>
+<script src="<?= assets_url() ?>admin/assets/pages/data-table/js/pdfmake.min.js"></script>
+<script src="<?= assets_url() ?>admin/assets/pages/data-table/js/vfs_fonts.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= assets_url() ?>admin/bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js"></script>
+
+<script>
+    // untuk datatable
+    var untukTabel = function() {
+        $('#datatable').DataTable();
+    }();
+
+    // untuk ambil data by id
+    var untukGetIdData = function() {
+        $(document).on('click', '#upd', function() {
+            var ini = $(this);
+
+            $.ajax({
+                type: "post",
+                url: "<?= admin_url() ?>/jabatan/get",
+                dataType: 'json',
+                data: {
+                    id: ini.data('id')
+                },
+                beforeSend: function() {
+                    ini.attr('disabled', 'disabled');
+                    ini.html('<i class="fa fa-spinner"></i>&nbsp;Menunggu...');
+                },
+                success: function(data) {
+                    $('#modal-input').modal({
+                        backdrop: 'static',
+                        keyboard: false
+                    })
+
+                    $('.modal-title').html('Ubah Jabatan');
+
+                    $('form').attr('action', '<?= admin_url() ?>/jabatan/upd');
+
+                    $('#inpkode').val(data.kd);
+                    $('#inpnama').val(data.nama);
+                    $('#inpketerangan').val(data.keterangan);
+                    $('#modal-input').modal('show');
+
+                    ini.removeAttr('disabled');
+                    ini.html('<i class="fas fa-edit"></i>&nbsp;Ubah');
+                }
+            });
+        });
+    }();
+
+    // untuk ambil data by id
+    var untukBersihkanModal = function() {
+        $(document).on('click', '#cls', function() {
+            var ini = $(this);
+
+            $('.modal-title').html('Tambah Jabatan');
+            $('#inpkode').val('<?= getKodeOtomatis('jabatan', 'JB') ?>');
+            $('#inpnama').val('');
+            $('#inpalamat').val('');
+            $('#inpketerangan').val('');
+        });
+    }();
+
+    // untuk simpan dan ubah data
+    var untukTambahDanUbahData = function() {
+        var parsleyConfig = {
+            errorsContainer: function(parsleyField) {
+                var $err = parsleyField.$element.siblings('#error');
+                return $err;
+            }
+        }
+
+        $('#form-add').parsley(parsleyConfig);
+
+        $('#form-add').submit(function(e) {
+            e.preventDefault();
+
+            $('#inpnama').attr('required', 'required');
+            $('#inpalamat').attr('required', 'required');
+            $('#inpketerangan').attr('required', 'required');
+
+            if ($('#form-add').parsley().isValid() == true) {
+                $.ajax({
+                    method: $(this).attr('method'),
+                    url: $(this).attr('action'),
+                    data: new FormData(this),
+                    contentType: false,
+                    processData: false,
+                    dataType: 'json',
+                    beforeSend: function() {
+                        $('#add').attr('disabled', 'disabled');
+                        $('#add').html('<i class="fa fa-spinner"></i>&nbsp;Menunggu...');
+                    },
+                    success: function(data) {
+                        swal({
+                                title: data.title,
+                                text: data.text,
+                                icon: data.type,
+                                button: data.button,
+                            })
+                            .then((value) => {
+                                location.reload();
+                            });
+                    }
+                })
+            }
+        });
+    }();
+</script>
